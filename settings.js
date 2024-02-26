caca81da668a53a30f45efff7c4aebd519f143d6
DERA10 <145374634+DERA10@users.noreply.github.com>
2024-02-26 10:59:19 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 //YouTube: @DGXeon
 //Instagram: unicorn_xeon13
 //Telegram: t.me/xeonbotinc
-//GitHub: @DGXeon
+//GitHub: @iamevilboy
 //WhatsApp: +916909137213
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 
@@ -12,29 +12,29 @@ const chalk = require('chalk')
 
 //owmner v card
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: Ednut001" //ur github or insta name
+global.socialm = "GitHub: iamevilboy" //ur github or insta name
 global.location = "Nigeria, Lagos, Abuja" //ur location
 
 //new
-global.botname = 'ednut bot md' //ur bot name
-global.ownernumber = '2347036214381' //ur owner number
-global.ownername = '𝐋𝐄𝐆𝐄𝐍𝐃𝐀𝐑𝐘 𝐄𝐃𝐍𝐔𝐓‎' //ur owner name
+global.botname = '𝐓𝐇𝐄 𝐉𝐎𝐊𝐄𝐑 𝐁𝐎𝐓' //ur bot name
+global.ownernumber = '2347010039405' //ur owner number
+global.ownername = '𝐀𝐍𝐓𝐈𝐒𝐎𝐂𝐈𝐀𝐋 𝐇𝐔𝐌𝐈𝐃𝐈𝐓𝐘‎' //ur owner name
 global.websitex = "https://chat.whatsapp.com/EduR0psW2l41eonnvegRuk"
-global.wagc = "https://chat.whatsapp.com/EduR0psW2l41eonnvegRuk"
-global.themeemoji = '👀'
-global.wm = " ednut Bot Inc."
+global.wagc = ""
+global.themeemoji = '🎃'
+global.wm = " joker Bot Inc."
 global.botscript = 'https://github.com/Ednut001/ednutBot-MD10' //script link
-global.packname = "Sticker By"
-global.author = "Ednut"
-global.creator = "2347036214381@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["2347036214381"] // Premium User
+global.packname = "𝑰𝒂𝒎𝒙𝒅𝒂"
+global.author = "𝑻𝒉𝒆 𝑱𝒐𝒌𝒆𝒓 𝑩𝒐𝒕"
+global.creator = "2347010039405@s.whatsapp.net"
+global.xprefix = '='
+global.premium = ["2347010039405"] // Premium User
 global.hituet = 0
 
 //bot sett
 global.typemenu = 'v2' // menu type 'v1' => 'v8'
 global.typereply = 'v3' // reply type 'v1' => 'v3'
-global.autoblocknumber = '92' //set autoblock country code
+global.autoblocknumber = '212' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = true //welcome/left in groups
 global.anticall = false //bot blocks user when called
@@ -44,10 +44,10 @@ global.groupevent = true //show update messages in group chat
 //msg
 global.mess = {
 	limit: 'Your limit is up!',
-	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+	nsfw: 'Nsfw is disabled in this group',
     done: 'Done bruh✓',
     error: 'Error!',
-    success: 'made by a legend named ednut🦄!'
+    success: '𝑮𝒆𝒏𝒆𝒓𝒂𝒕𝒆𝒅 𝒃𝒚 𝑱𝒐𝒌𝒆 𝒃𝒐𝒕🥇!'
 }
 //thumbnail
 global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
